ae5c3a6ce2bf091fff81985b717e8d7e000774f3
Christoffer <christoffer.isenberg@gmail.com>
2024-09-02 10:25:23 +0200

    ovning 1

---
+++ ovn1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ovn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 5; ++i)
+            {
+                for (int j = 1; j <= 5; ++j)
+                {
+                    var number = i * j;
+                    Console.Write($"{number.ToString().PadRight(3)}");
+
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+}
