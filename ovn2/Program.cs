7306ae19ee3873321a7c16f3d659ef81f51f887e
Christoffer <christoffer.isenberg@gmail.com>
2024-09-02 10:44:02 +0200

    ovn 2

---
+++ ovn2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ovn2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 4;
+            int height = 4;
+            for (int i = 1; i <= height; ++i)
+            {
+                for (int j = 1; j <= width; ++j)
+                {
+                    var number = i * j;
+                    Console.Write($"#".PadRight(2));
+
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+}
